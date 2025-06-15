7f864c2075d33a61e940c5cdb6352491dadb8ef9
oniafk <manufelipearias@gmail.com>
2025-06-15 14:04:00 +1000

    getting user info

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,21 @@
 import styled from "styled-components";
+
+import { useAuth } from "../hooks/useAuth";
+
 export function Home() {
+  const { signOut } = useAuth();
+  const { user } = useAuth();
+  console.log("User in Home:", user);
   return (
     <Container>
       <h1>Home</h1>
+      <button onClick={signOut}> Sign out</button>
+      {user && (
+        <div>
+          <p>Welcome, {user.user_metadata.full_name}!</p>
+          <p>User email: {user.email}</p>
+        </div>
+      )}
     </Container>
   );
 }
